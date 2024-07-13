510d37ec164a9cdcf5fd552eee8ae886f2263919
80548262 <74823878+80548262@users.noreply.github.com>
2024-07-13 12:23:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -202,8 +202,15 @@ def settings():
         return redirect(url_for('index'))
     return render_template('settings.html')
 
-
-
+@app.cli.command() # 注册为命令，可以传入 name 参数来自定义命令
+@click.option('--drop', is_flag=True, help='Create after drop.') # 设置选项
+def initdb(drop):
+    """Initialize the database."""
+    if drop: # 判断是否输入了选项
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.') # 输出提示信息
+    
 
 if __name__ == '__main__':
     app.run()
